01e41b8249d5f37dbee8964af7fa40bb64af6481
AlintaAnnWilson <alinta.wilson248@gmail.com>
2017-04-16 15:21:51 +0530

    Update pycam.py

--- pycam.py
+++ pycam.py
@@ -1,38 +1,47 @@
-"""
-StreamBox. Core file pycam.py.
-
-Current capability: Flip camera, record raw h264 video, convert raw file to mkv or mp4, remove 
-raw file (temp file), display progress.
-
-features to add: Accept gpio input, add audio, custom/time dependent file names, syncing files 
-to Google Drive (integrating Grive), making pycam.py into a service.
-"""
 import picamera
-from time import sleep
+#from time import sleep
 from subprocess import call
 import os
+import RPi.GPIO as GPIO
+import time
 
-#Shell code for converting video. 
-video_convert = "ffmpeg -r 30 -i video.h264 -vcodec copy outputfile.mp4"
+video_convert = "ffmpeg -r 30 -i video.h264 -vcodec copy gdrive/outputfile6.mp4"
 
 os.system('clear')
 print('Recording...')
+#Lighting LED
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(40, GPIO.OUT)
+for i in range(0,10):
+    GPIO.output(40,1)
+    time.sleep(1)
+    GPIO.output(40,0)
+    time.sleep(1)
 
+#Recording Video
 camera = picamera.PiCamera()
 camera.hflip = True
 camera.vflip = True
 camera.start_recording('video.h264')
-#30 for 30 second long video. Make it a variable for custom video length.
-sleep(30)
+sleep(5)
 camera.stop_recording()
+
+    #For turning off LED
+    GPIO.output(40,0)
+
+
 os.system('clear')
 print('Recording...... \nStopping recording')
 
 sleep(1)
+#os.system('DATE=$(date +"%Y-%m-%d_%H%M  ffmpeg -r 30 -i video.h264 -vcodec copy $DATE.mp4')
 call ([video_convert], shell=True)
 os.system('clear')
 print('Recording..............[y] \nStopping recording.....[y] \nConverting File........[y]')
 print('Deleting temp files....[y]')
 os.system('rm video.h264')
+print('Uploading')
+os.system('rclone copy /home/pi/pythoncam/gdrive googledrive:gdrive')
+os.system('clear')
+print('Recording..............[y] \nStopping recording.....[y] \nConverting.............[y] \nDeleting temp files....[y] \nUploading..............[y]')
 print('\nCompleted')
-
