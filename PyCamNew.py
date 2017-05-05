f478390460f8f4438a5063cf396c580ee2e428a3
PrinceShaji <shaji.prince@yandex.com>
2017-05-05 07:26:13 +0530

    Update PyCamNew.py

--- PyCamNew.py
+++ PyCamNew.py
@@ -9,14 +9,15 @@ import os
 import RPi.GPIO as GPIO
 from time import time, sleep, strftime, gmtime
 from multiprocessing import Process
+import itertools
 
-video_convert = "ffmpeg -r 30 -i video.h264 -vcodec copy outputfile.mp4"
+video_convert = "ffmpeg -r 30 -i video.h264 -vcodec copy outputfile.mp4" 
 #video_convert = "ffmpeg -r 30 -i video.h264 -vcodec copy %s.mp4" %DateFilename
 
 DateFilename=strftime("%Y-%m-%d %H:%M", gmtime())
 
 def RecordVideo():
-    camera = picamera.PiCamera()
+	camera = picamera.PiCamera()
     camera.hflip = True
     camera.vflip = True
     camera.start_recording('video.h264')
@@ -30,6 +31,7 @@ def RecordVideo():
 def StopRecording():
 	camera = picamera.PiCamera()
 	camera.stop_recording()
+
 
 def ConvertVideo():
 	call ([video_convert], shell=True)
@@ -52,7 +54,22 @@ def UploadFiles():
 
 	"""
 	command = ([RCLONE, 'move', '--log-file=rclone_upload.log', '--transfers', RCLONE_TRANSFERS, '--drive-chunk-size=16M', '--exclude', 'filepart', LOCAL_DIR + dir + '/', REMOTE_NAME  + REMOTE_DIR + dir + '/'])
-        result = subprocess.Popen(command)
-        result.communicate()
-        """
-	
+    result = subprocess.Popen(command)
+    result.communicate()
+    """
+
+
+def LedStandby():
+	GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(40, GPIO.OUT)
+    for num in itertools.count(1): #creates an infinite loop
+        GPIO.output(40,1)
+        sleep(1)
+        GPIO.output(40,0)
+        sleep(1)
+
+
+
+
+
+if __name__ == "__main__":   #start of the program?        
